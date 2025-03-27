9bdf69df99ad9cbd24ee399028ee79b8abdd724f
techtorialacademy <yusufcanturan@icloud.com>
2025-03-26 19:34:38 -0500

    Update on GItAutomation Class

--- src/main/java/GitAutomationCode.java
+++ src/main/java/GitAutomationCode.java
@@ -1,5 +1,7 @@
 public class GitAutomationCode {
     public static void main(String []args){
         System.out.println("This class is created for git automation purposes.");
+        System.out.println("This class should not be changed as it is" +
+                "used commonly.");
 
     }}
